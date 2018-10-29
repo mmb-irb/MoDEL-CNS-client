7676435a1d04d18f160c63dd3382586d7af39526
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2018-10-29 14:57:05 +0100

    added footer

--- src/layout/footer/index.js
+++ src/layout/footer/index.js
@@ -1,15 +1,35 @@
 import React, { PureComponent } from 'react';
-import { Paper, Toolbar, Typography } from '@material-ui/core';
+import { Paper, Toolbar } from '@material-ui/core';
+
+import style from './style.module.css';
+
+import logoHBP from '../../images/logo-hbp.png';
+import logoIRB from '../../images/logo-irb.png';
 
 export default class Footer extends PureComponent {
   render() {
     return (
       <footer>
         <Paper position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit">
-              Footer
-            </Typography>
+          <Toolbar className={style.footer}>
+            <a
+              href="https://www.humanbrainproject.eu/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img
+                height="100px"
+                src={logoHBP}
+                alt="Human Brain Project website"
+              />
+            </a>
+            <a
+              href="https://www.irbbarcelona.org/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <img height="100px" src={logoIRB} alt="IRB Barcelona website" />
+            </a>
           </Toolbar>
         </Paper>
       </footer>
