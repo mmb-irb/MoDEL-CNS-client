89f509cdf406a7c933d973caad3ccf1e3e27dd65
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2018-12-03 15:16:59 +0100

    change highlight component

--- src/components/highlight/index.js
+++ src/components/highlight/index.js
@@ -1,21 +1,20 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { escapeRegExp } from 'lodash-es';
 
-export default class Highlight extends PureComponent {
-  render() {
-    const { highlight, children } = this.props;
-    if (!highlight || typeof children !== 'string') return children || null;
-    const re = new RegExp(`(${escapeRegExp(highlight)})`, 'i');
-    const text = (children || '').toString();
-    return text
-      .split(re)
-      .filter(Boolean)
-      .map((chunk, index) =>
-        React.createElement(
-          re.test(chunk) ? 'mark' : 'span',
-          { key: index },
-          chunk,
-        ),
-      );
-  }
-}
+export default React.memo(({ highlight, children }) => {
+  if (!highlight || typeof children !== 'string') return children || null;
+
+  const re = new RegExp(`(${escapeRegExp(highlight)})`, 'i');
+
+  return (children || '')
+    .toString()
+    .split(re)
+    .filter(Boolean)
+    .map((chunk, index) =>
+      React.createElement(
+        re.test(chunk) ? 'mark' : 'span',
+        { key: index },
+        chunk,
+      ),
+    );
+});
