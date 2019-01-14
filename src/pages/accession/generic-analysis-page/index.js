c6659aa23dde77bb098e462c060eb9641fd20d12
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-01-14 16:21:21 +0100

    remove a log

--- src/pages/accession/generic-analysis-page/index.js
+++ src/pages/accession/generic-analysis-page/index.js
@@ -28,7 +28,6 @@ const Analysis = ({
     `${BASE_PATH}${accession}/analyses/${analysis}/`,
   );
 
-  if (!loading && payload) console.log(Object.keys(payload.y));
   return (
     <Suspense fallback={<span>Loading</span>}>
       <Card className={style.card}>
