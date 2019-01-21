a24b73b35d627dee785584c9f058fa1216d1aeea
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-01-21 14:44:06 +0100

    get basename from config

--- src/App/index.js
+++ src/App/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Root from '../Root';
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.REACT_APP_ROOT}>
     <Root />
   </Router>
 );
