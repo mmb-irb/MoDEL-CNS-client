26ed1ad3e981ff99b8c8ce1a5f172735d6d97875
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-01-21 16:01:52 +0100

    revert previous changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { MemoryRouter as Router } from 'react-router-dom';
 
 import Root from '../Root';
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <Root />
   </Router>
 );
