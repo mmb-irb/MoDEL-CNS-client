3054cf24e8ea63f9bbc27909c89a0c5f4426f726
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-02-26 12:28:14 +0100

    add connection level module

---
+++ src/utils/connection-level/index.js
@@ -0,0 +1,23 @@
+export const LOW = Symbol('low');
+export const MEDIUM = Symbol('medium');
+export const HIGH = Symbol('high');
+
+const connectionLevel = () => {
+  if (!(navigator && 'connection' in navigator)) return MEDIUM;
+  const { effectiveType, saveData } = navigator.connection;
+  if (saveData === true) return LOW;
+  if (saveData || effectiveType) return MEDIUM;
+  switch (effectiveType) {
+    case 'slow-2g':
+    case '2g':
+      return LOW;
+    case '3g':
+      return MEDIUM;
+    case '4g':
+      return HIGH;
+    default:
+      return HIGH;
+  }
+};
+
+export default connectionLevel;
