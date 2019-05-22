300ddaaf40d76d65802ce2c9c7825ea55794b50c
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-05-22 15:46:26 +0200

    added ability to do snapshot tests on images

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { configure, mount } from 'enzyme';
 import AdapterForReact16 from 'enzyme-adapter-react-16';
+import { toMatchImageSnapshot } from 'jest-image-snapshot';
+
+import { Readable } from 'stream';
 
 // necessary set up
 configure({ adapter: new AdapterForReact16() });
+
+// we extend the extension, to also have it understand ReadableStreams
+expect.extend({
+  toMatchImageSnapshot(imageOrStream, ...args) {
+    const boundMatcher = toMatchImageSnapshot.bind(this);
+    if (!(imageOrStream instanceof Readable)) {
+      return boundMatcher(imageOrStream, ...args);
+    }
+    // we have a stream, use it first to pass the complete buffer to the matcher
+    const parts = [];
+    imageOrStream.on('data', chunk => parts.push(chunk));
+    return new Promise((res, rej) => {
+      imageOrStream.on('error', rej);
+      imageOrStream.on('end', () => {
+        res(boundMatcher(Buffer.concat(parts), ...args));
+      });
+    });
+  },
+});
 
 // utils
 const TestHookComponent = ({ callback }) => {
