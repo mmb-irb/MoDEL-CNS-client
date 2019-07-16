905d677904038a5207143505a020d870cf7ae2ca
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-07-16 11:27:01 +0200

    fix header active marker overflowing

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -3,17 +3,18 @@ import { AppBar, Toolbar } from '@material-ui/core';
 
 import Menu from '../../components/menu';
 
+import style from './style.module.css';
 import logo from '../../images/logo.png';
 
 const Header = () => (
   <AppBar position="sticky">
-    <Toolbar>
+    <Toolbar className={style.toolbar}>
       <img
         src={logo}
         width="200px"
         height="102.5px"
         loading="lazy"
-        alt="MoDEL - Centra Nervous System - Logo"
+        alt="MoDEL - Central Nervous System - Logo"
       />
       <Menu />
     </Toolbar>
