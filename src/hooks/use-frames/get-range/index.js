2af3f710c1be4391d6fd4c138e976c6d40889ec2
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-07-18 15:54:29 +0200

    added way to merge ranges when contiguous

--- src/hooks/use-frames/get-range/index.js
+++ src/hooks/use-frames/get-range/index.js
@@ -1,6 +1,28 @@
 const getRange = frames => {
   if (!frames || !frames.length) return;
-  return `frames=${frames.map(frame => `${frame}-${frame}`).join(',')}`;
+
+  const parts = [];
+  let currentStart, currentEnd;
+  for (const [index, frame] of frames.entries()) {
+    // if this frame is NOT the one immediately after the previous one
+    if (frame - 1 !== frames[index - 1]) {
+      // and it's not the first time
+      if (index) {
+        // push the range string to the array of parts
+        parts.push(`${currentStart}-${currentEnd}`);
+      }
+      // and set a new current start
+      currentStart = frame;
+    }
+    // also, update the last frame that we keep track of
+    currentEnd = frame;
+  }
+  // push the rest of the values at the end of the loop
+  parts.push(`${currentStart}-${currentEnd}`);
+
+  const output = `frames=${parts.join(',')}`;
+
+  return output;
 };
 
 export default getRange;
--- src/hooks/use-frames/get-range/index.test.js
+++ src/hooks/use-frames/get-range/index.test.js
@@ -10,4 +10,10 @@ describe('getRange', () => {
     expect(getRange([0])).toBe('frames=0-0');
     expect(getRange([2, 4])).toBe('frames=2-2,4-4');
   });
+
+  it('should merge contiguous ranges', () => {
+    expect(getRange([0, 1])).toBe('frames=0-1');
+    expect(getRange([0, 1, 2, 4])).toBe('frames=0-2,4-4');
+    expect(getRange([0, 1, 2, 3, 4])).toBe('frames=0-4');
+  });
 });
