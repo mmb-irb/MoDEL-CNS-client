ef8fedcb860d4209bcb253acf63c10d286911695
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-07-18 15:46:53 +0200

    add AbortController polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
+
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
