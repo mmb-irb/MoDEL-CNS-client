c45bd93365c63f9ee6735763ff0f8e691e5cbb8d
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-09-30 10:51:42 +0200

    more comments

--- style-embedder/index.js
+++ style-embedder/index.js
@@ -8,6 +8,10 @@ const styleLinkRE = /<link[^>]+\.css[^>]+>/gim;
 const hrefRE = /href="\.(?<href>[^"]+)"/i;
 const relativeURLInCSS = /url\(\.\.\/\.\.\//g; // matches: url(../../
 
+/**
+ * This logic is aimed at improving render by injection blocking style inside
+ * the index.html document, instead of having to do additional network requests
+ */
 const main = async () => {
   // read index.html file content
   let indexFile = await asyncReadFile('./build/index.html', {
@@ -19,19 +23,20 @@ const main = async () => {
     const { href } = (link.match(hrefRE) || []).groups || {};
     // read stylesheet file content
     let style = await asyncReadFile(`./build/${href}`, { encoding: 'utf8' });
-    // fix relative urls
+    // fix relative urls since they won't be relative to CSS files anymore
     style = style.replace(relativeURLInCSS, 'url(./');
     // but then, we have to add the path to the sourcemaps
     style = style.replace(
       /sourceMappingURL=/g,
       'sourceMappingURL=./static/css/',
     );
-    // inline content of stylesheet inside html
+    // inline content of stylesheet inside html content
     indexFile = indexFile.replace(link, `<style>${style}</style>`);
   }
-  // output resulting index.html file (overwrite)
+  // output resulting index.html file (by overwriting it)
   await asyncWriteFile('./build/index.html', indexFile, { encoding: 'utf8' });
 
+  // return a message of what we just did
   return '⚡ Injected embeddable stylesheets inside "build/index.html" file ⚡';
 };
 
