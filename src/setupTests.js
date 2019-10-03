f74ce805efec690854394fc9ebe961700c526181
Aurélien Luciani <aurelien.luciani@irbbarcelona.org>
2019-10-03 15:22:29 +0200

    add mock for matchMedia

--- src/setupTests.js
+++ src/setupTests.js
@@ -27,6 +27,16 @@ expect.extend({
   },
 });
 
+window.matchMedia = jest.fn().mockImplementation(query => {
+  return {
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+  };
+});
+
 // utils
 const TestHookComponent = ({ callback }) => {
   callback();
