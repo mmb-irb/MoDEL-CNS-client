825daab2df496da5e84bc0edd52ea5d98c9a3e38
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2022-03-04 12:09:40 +0100

    fixed typo in acknowledgements

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -84,7 +84,7 @@ const Home = () => (
           </Typography>
           <br />
           This open source software code was developed in part in the Human
-          Barin Project, funded from the European Union's Horizon 2020 Framework
+          Brain Project, funded from the European Union's Horizon 2020 Framework
           Programme for Research and Innovation under Specific Grant Agreements
           No. 720270, No. 785907 and No. 945539 (Human Brain Project SGA1, SGA2
           and SGA3).
